f7ff4dd3e2343f22173df06fe0d635ee47c4d4b2
JuanManueArenas <juanarenas@colegioguanenta.edu.co>
2023-02-22 07:36:31 -0500

    se agrego el diagrama al README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mayor_2enteros
 Programa para verificar cual de los numeros enteros es el mayor
+
+# Diseño
+
+## Diagrama de flujo 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ mayor_2enteros.py
@@ -0,0 +1,23 @@
+print("------------------------------------------")
+print("---El resultado de las operaciones son:---")
+print("------------------------------------------")
+
+
+# input
+
+minutos = int(input("ingrese los minutos de la llamada: "))
+
+# procesing
+
+if x>y:
+    costo = 300
+
+else: 
+    costo = (minutos * 50 ) + 300 - 150
+
+
+
+
+# output
+
+print("el costo de la llamada es: " + str (costo))
